e8c436e2e37eafa3f89051c9de90acb831995fd3
Laido82 <tebabkhaelaid@gmail.com>
2024-04-13 13:26:39 +0200

    Add county,Latitude and longitude.

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ def get_weather():
     if weather_data['cod'] != 200:
         return render_template('city_not_found.html')
 
-    return render_template('weather.html',  city=weather_data['name'].capitalize(), status=weather_data['weather'][0]['description'].capitalize(), temp=f'{(weather_data["main"]["temp"]-32)/1.8:.2f}', feels_like=f'{(weather_data["main"]["feels_like"]-32)/1.8:.2f}')
+    return render_template('weather.html',  city=weather_data['name'].capitalize(), country=weather_data["sys"]["country"].capitalize(), latitude=weather_data["coord"]["lat"], longitude=weather_data["coord"]["lon"], status=weather_data['weather'][0]['description'].capitalize(), temp=f'{(weather_data["main"]["temp"]-32)/1.8:.2f}', feels_like=f'{(weather_data["main"]["feels_like"]-32)/1.8:.2f}')
 
 
 if __name__ == "__main__":
