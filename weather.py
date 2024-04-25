aeef7a780974305d6782474e0dba2ca017af9e21
Laido82 <tebabkhaelaid@gmail.com>
2024-04-25 19:26:33 +0200

    Fix bugs in weather.py

--- weather.py
+++ weather.py
@@ -6,8 +6,7 @@ import requests
 load_dotenv()
 
 
-def get_current_weather(city="Kansas"):
-    # request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+def get_current_weather(city: str):
     request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
     weather_data = requests.get(request_url).json()
     return weather_data
@@ -16,12 +15,15 @@ def get_current_weather(city="Kansas"):
 if __name__ == "__main__":
     print('\n*** Get Weather Conditions ***\n')
     city = input('\n Please enter a city name : ')
+    weather_data = get_current_weather(city)
+
+    if weather_data['cod'] != 200:
+        print('City not found')
+
+    if city and weather_data['cod'] == 200:
+        pprint(weather_data)
 
     if not bool(city.strip()):
         city = "Algiers"
         weather_data = get_current_weather(city)
-        print('\n')
         pprint(weather_data)
-    weather_data = get_current_weather(city)
-    if weather_data['cod'] != 200:
-        print('City not found')
